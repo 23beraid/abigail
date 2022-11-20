1ea649c64bfc445cc6291a3c99c03fff6cc85028
23beraid <64813542+23beraid@users.noreply.github.com>
2022-11-19 19:25:59 -0500

    Initialize GitHub repository

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\23dhisne                                         */
+/*    Created:      Thu Sep 29 2022                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    10, 20          
+// Controller1          controller                    
+// Motor1               motor         1               
+// MotorGroup4          motor_group   4, 7            
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
