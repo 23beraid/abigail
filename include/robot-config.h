bcce53666509694a8127ea5e7170bffc2413ba87
Grungble <118462674+Grungble@users.noreply.github.com>
2023-01-13 11:34:37 -0500

    Update robot-config.h
    This is just here to make sure the brain knows what's there ig idk but It needs to be here

--- include/robot-config.h
+++ include/robot-config.h
@@ -3,13 +3,22 @@ using namespace vex;
 extern brain Brain;
 
 // VEXcode devices
+extern competition Competition;
 extern drivetrain Drivetrain;
 extern controller Controller1;
-extern motor Catapult;
-extern motor Lift;
-extern motor Motor6;
-extern motor Catapult2;
-/**
+extern motor Flywheel;
+extern motor intake;
+extern motor RightDrive1;
+extern motor RightDrive2;
+extern motor RightDrive3;
+extern motor_group RightDrive;
+extern motor LeftDrive1;
+extern motor LeftDrive2;
+extern motor LeftDrive3;
+extern motor_group LeftDrive;
+extern digital_out Solenoid;
+extern digital_out Expansion;
+extern digital_out Expansion2;
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
  * 
  * This should be called at the start of your int main function.
