0eb8405185e84e000a07d5cc15d884dcc5c3df17
Grungble <118462674+Grungble@users.noreply.github.com>
2023-01-27 13:53:06 -0500

    Removing Expansion Command
    I accidentally added this command to the normal auton code, it would set the expansion off if used

--- src/main.cpp
+++ src/main.cpp
@@ -140,8 +140,7 @@ void autonomous(void) {
   Drive.spinFor(forward, 3, rotationUnits::rev);
   LeftDrive.spinFor(forward, 3, rotationUnits::rev, false);
   RightDrive.spinFor(forward, 3, rotationUnits::rev, true);
-  wait(1, sec);
-  Expansion = true;
+ 
 
   // ..........................................................................
   // Insert autonomous user code here.
