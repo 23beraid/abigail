e92406486828602455b82588f280727a9dc56868
Grungble <118462674+Grungble@users.noreply.github.com>
2023-01-27 13:54:51 -0500

    I'm checking something

--- src/main.cpp
+++ src/main.cpp
@@ -140,7 +140,8 @@ void autonomous(void) {
   Drive.spinFor(forward, 3, rotationUnits::rev);
   LeftDrive.spinFor(forward, 3, rotationUnits::rev, false);
   RightDrive.spinFor(forward, 3, rotationUnits::rev, true);
- 
+  wait(1, sec);
+ Expansion = true;
 
   // ..........................................................................
   // Insert autonomous user code here.
